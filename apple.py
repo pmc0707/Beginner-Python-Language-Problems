dcc029145e352bc947848b82c9f15148b7b8aa6d
Pranjal0111 <133362644+Pranjal0111@users.noreply.github.com>
2023-05-20 20:16:09 +0530

    Add files via upload

---
+++ apple.py
@@ -0,0 +1,16 @@
+a=int(input("enter the certain number of apples"))
+if(a==7):
+    print("he can pick all of them")
+elif a==5:
+   print("1 apple is left behind")
+elif a==6 :
+   print("1 apple is left behind")
+elif a==4:
+   print("1 apple is left behind")
+else:
+   print("Abhishek")
+   
+                     
+
+
+   
